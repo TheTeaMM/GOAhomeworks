9ed8338a3650f4f80499c8d51d6ccea93ce2d693
TheTeaMM <buba.ebralidze18@gmail.com>
2023-11-25 15:14:52 +0400

    first commit

---
+++ day 2/homework.py
@@ -0,0 +1,10 @@
+mothersName = "diana"
+mothersage = 34
+fathersName = "Mamuka"
+fathersAge = 40
+brothersName = "Saba"
+brothersAge = 7
+
+print (mothersage + 5)
+print (fathersAge + 5)
+print (brothersAge + 5)
---
+++ day 3/practic.py
@@ -0,0 +1,33 @@
+name = "Buba"
+
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "Buba" არის ცვლადისთვის მნიშვნლებობა
+
+
+
+surname = "Ebralidze"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+
+name = "Buba"#ეს არის str(string) ტიპის ცვლადი
+age = "12" #ეს არის int ( integr ) მთელი რიცხვი
+height = 48.4 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#booLean (booL) ტიპის ცვლადი
+knows_programming = True #true ან False
+good_at_math = True #snakecase ( უბრალოდ წერის სტილი სტანდარტულად)
+goodatmath = True #ჯაოსკრიპტის cameLcase
+
+print(name + " " + surname)
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+#print (type(age))
+#print (type(name))
+#print (type(surname))
+#print (type(height))
+#print (type(knows_programming))
+
+print(name + " " + str(age))
