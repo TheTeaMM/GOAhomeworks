2cf69452c57b6993b8a65e76f464da2624ddc20d
TheTeaMM <buba.ebralidze18@gmail.com>
2024-03-28 22:33:29 +0400

    Homework

---
+++ day Folder/Day 41/script.js
@@ -0,0 +1,13 @@
+// დავალება 1
+let name = 'Buba';
+let age = '12';
+let date = '18.01.2012'
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof date);
+// დავალება 2 
+console.log('Hello, I am', age, ' My name is', name , 'I Bown in' , date );
+// დავალება 3 
+let num = 15;
+let text = num.toString();
