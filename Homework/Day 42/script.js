b7ae8976521aed174c096646ba8fff6953b2d00b
TheTeaMM <buba.ebralidze18@gmail.com>
2024-04-03 22:37:11 +0400

    Homework

---
+++ Homework/Day 42/script.js
@@ -0,0 +1,39 @@
+// 2 დავალება
+console.log(5>3)
+console.log(8>3)
+console.log(4>80)
+console.log(100>3)
+console.log(5>3)
+console.log(5>8)
+console.log(6>3)
+console.log(5>3)
+console.log(10>33)
+console.log(5>13)
+//3 დავალება
+let x = "2"
+let y = "1"
+y = y++
+x = x--
+console.log(y)
+console.log(++x)
+console.log(--y)
+console.log(++x)
+console.log(++x)
+console.log(--y)
+console.log(++x)
+console.log(++x)
+console.log(--y)
+console.log(++x)
+//4 დავალება
+let a = 5;
+let b= ++a;
+
+let c = 5;
+let d= ++c;
+
+let g = 5;
+let e= ++g;
+
+let  f= 5;
+let H= ++f;
+
