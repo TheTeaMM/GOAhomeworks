3ecb6df57b1a4dd65119a1660ba36554942e5789
TheTeaMM <buba.ebralidze18@gmail.com>
2024-04-06 22:20:31 +0400

    Sololearn

---
+++ Classwork/Day 03/script.js
@@ -0,0 +1,24 @@
+// ou want to withdraw some money from your bank account.
+
+// The program takes two numbers as input, your account balance and the amount you want to withdraw, and assigns them to corresponding "balance" and "withdraw" variables.
+
+// Task
+
+// Calculate and output the remaining balance after the withdrawal.
+
+// Sample Input
+
+// 450000
+
+// 9000
+
+// Sample Output
+
+// 441000
+
+// Explanation
+
+// The first two lines of the given code declare the balance and the withdraw variables, whose values will be automatically taken as input and will be used in test cases. This means that your solution will be tested with different values of those variables.
+
+// Don't change that part of the code and treat it as a usual variable.
+console.log(4500-2)
