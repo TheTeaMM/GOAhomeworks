60c181212612ddd03afb70bf05a3009c4e6b00e5
TheTeaMM <buba.ebralidze18@gmail.com>
2024-04-10 22:11:18 +0400

    Lol

---
+++ Homework/Day 44/script.js
@@ -0,0 +1,52 @@
+// Homework #1
+function Math(math1,math2) {
+    console.log(math1+math2)
+}
+
+Math(5,2)
+
+function Math(math3,math4) {
+    console.log(math3+math4)
+}
+
+Math(5,2)
+
+function Math(math5,math6) {
+    console.log(math5+math6)
+}
+
+Math(5,2)
+
+function Math(math7,math8) {
+    console.log(math7+math8)
+}
+
+Math(5,2)
+
+// Homework #2
+function rectangle(Height,Width){
+
+    console.log(`is the area of the rectangle ${Number(Height*Width)}`)
+}
+
+rectangle(5,10)
+// Homework #3
+function goa() {
+    console.log('goabest')
+}
+goa()
+goa()
+goa()
+goa()
+goa()
+goa()
+goa()
+goa()
+goa()
+goa()
+//Homework #5
+//``
+function family (Mynameage = 'Buba 12' , Mydadnameage = '42 Mamuka' , Mymomnameage = '34 Diana' , Mybrothenamerage = '8 Saba') {
+    console.log(`hello my name  and age is ${Mynameage} My dad name and age is ${Mydadnameage} My Mom name and age is ${Mymomnameage} My brother name and age is ${Mybrothenamerage}`)
+}
+family()
