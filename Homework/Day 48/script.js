aaeb8e3550a609022767294156e9210f39b2583e
TheTeaMM <buba.ebralidze18@gmail.com>
2024-04-26 17:15:39 +0400

    Homewrok

---
+++ Homework/Day 48/script.js
@@ -0,0 +1,18 @@
+let user1 = {
+    name:'Bubas',
+    surname:'tetri',
+    age:12,
+    adress:'aasdar'
+}
+let a = Number(prompt('How long will you travel in the future?'))
+
+alert(`you will be ${user1.age+a}`)
+
+
+
+
+
+user1.name = "Buba"
+user1.surname = "Ebraldze"
+user1.age = 12
+user1.adress = "Angisa"
