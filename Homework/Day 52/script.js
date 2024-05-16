84b9de62679801ff608071ff2ee976e675efdac5
TheTeaMM <buba.ebralidze18@gmail.com>
2024-05-16 17:35:27 +0400

    Homework

---
+++ Homework/Day 52/script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("div1").innerHTML = "Hello World!";
+document.getElementById("div1").style.height = "50px";
+document.getElementById("div1").style.width = "90px";
+document.getElementById("div1").style.backgroundColor = "red";
+document.getElementsByClassName("div1")
+document.querySelectorAll("div1")
