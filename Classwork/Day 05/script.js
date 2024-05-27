f96b76fc8f14d8d57c2d021d5d43325a25d52d6f
TheTeaMM <buba.ebralidze18@gmail.com>
2024-05-27 20:38:31 +0400

    Homework

---
+++ Classwork/Day 05/script.js
@@ -0,0 +1,9 @@
+function Person(firstName, lastName, age, Hobby, Proffesion) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.Hobby = Hobby;
+    this.Proffesion = Proffesion;
+    
+  }
+  
---
+++ Homework/Day 73/script.js
@@ -0,0 +1,7 @@
+const book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+    getSummary: function(){
+        return this.book1+ ' ' + this.book2 + ' ' + this.book3}
+  }
