1b508719683fe28af80d696e8df6b301bd5455fd
TheTeaMM <buba.ebralidze18@gmail.com>
2024-05-28 19:30:29 +0400

    Homework

--- Homework/Day 73/script.js
+++ Homework/Day 73/script.js
@@ -5,3 +5,8 @@ const book = {
     getSummary: function(){
         return this.book1+ ' ' + this.book2 + ' ' + this.book3}
   }
+
+console.log(book.title)
+
+book.year = 2021
+console.log(book.year)
