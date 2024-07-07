033e5add7455755bbdf24f111b5b00a1cc8cd330
TheTeaMM <buba.ebralidze18@gmail.com>
2024-07-07 22:20:05 +0400

    Homework

--- Homework/Day 81/Homework/script.js
+++ Homework/Day 81/Homework/script.js
@@ -1,9 +1,46 @@
-
+//1
 let cvladi = document.getElementById("btn")
 let cvladi2 = document.getElementById("p")
-let random = Math.floor(Math.random() * 10 )
 
 cvladi.addEventListener("click" , function(){
-     cvladi2.textContent = random
-     
+      cvladi2.textContent = Math.floor(Math.random() * 1000 )
 })
+//2
+let btn = document.getElementById("btn2")
+let p2 = document.getElementById("p2")
+let body = document.getElementById("bd")
+
+
+let area = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+btn.addEventListener("click" , function(){
+
+    for(let i = 0 ; i<6; i++){
+        let hex = "#"
+        hex += area[Math.trunc(Math.random()*area.length)]
+    }
+    p2.textContent = hex
+    body.style.backgroundColor = hex
+})
+
+
+// let text = document.getElementById('HaxText')
+
+// let body = document.getElementById('body')
+
+// let click = document.getElementById('me')
+
+// let haxText = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+// click.addEventListener('click', function(){
+//     let hax = '#'
+//   for(let i = 0;i<6;i++){
+//      hax += haxText[Math.trunc(Math.random()*haxText.length)]
+//   }
+
+//  text.textContent = hax
+//  body.style.backgroundColor = hax
+// }
+ 
+
+// )
--- Homework/Day 82/ecommerce-product-page-main/script.js
+++ Homework/Day 82/ecommerce-product-page-main/script.js
@@ -4,3 +4,19 @@ function changeImage() {
     mainImage.alt = 'New Image'; // Необязательно, можно поменять атрибут alt
 }
 
+
+let count = 0;
+let rslt = document.getElementById('rslt')
+let plus = document.getElementById('plus')
+let minus = document.getElementById('minus')
+
+plus.onclick = function(){
+    count++;
+    rslt.textContent = count;
+    
+}
+minus.onclick = function(){
+    count--;
+    rslt.textContent = count;
+    
+}
