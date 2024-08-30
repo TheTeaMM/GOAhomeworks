757dd7730239552c7dff4e20a10834e0b9d672a5
TheTeaMM <buba.ebralidze18@gmail.com>
2024-08-30 12:01:45 +0400

    homework

--- Homework/Day 81/Homework/script.js
+++ Homework/Day 81/Homework/script.js
@@ -6,22 +6,18 @@ cvladi.addEventListener("click" , function(){
       cvladi2.textContent = Math.floor(Math.random() * 1000 )
 })
 //2
-let btn = document.getElementById("btn2")
-let p2 = document.getElementById("p2")
-let body = document.getElementById("bd")
+let btn = document.getElementById("btn2");
+let p = document.getElementById("p2");
+let area = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 
-
-let area = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-
-btn.addEventListener("click" , function(){
-
-    for(let i = 0 ; i<6; i++){
-        let hex = "#"
-        hex += area[Math.trunc(Math.random()*area.length)]
+btn.addEventListener("click", function() {
+    let hex = "#";
+    for (let i = 0; i < 6; i++) {
+        hex += area[Math.trunc(Math.random() * area.length)];
     }
-    p2.textContent = hex
-    body.style.backgroundColor = hex
-})
+    p.textContent = hex;
+    document.body.style.backgroundColor = hex;
+});
 
 
 // let text = document.getElementById('HaxText')
